4cf617259cfedb80b5d53d612677f0d7a7268cf9
Adammmmmm16516 <belkhatiradam40@gmail.com>
2024-12-23 18:48:44 +0100

    ajout du rang dans data3

--- src/scraping.py
+++ src/scraping.py
@@ -4,6 +4,7 @@ import pandas as pd
 import re
 
 df = pd.read_csv('data_cleaned.csv')
+df3 = pd.read_csv('data3.csv')
 
 #Mesure de l'intérêt des consommateurs
 def extract_item_info(IDitem):
@@ -35,18 +36,18 @@ def extract_item_info(IDitem):
     }
 
 #Modification de la table pour ajouter le coefficient  
-L_coeff=[]
+# L_coeff=[]
 
-for index, row in df.iterrows():
-    IDitem = row["ID"].split('|')[1] 
-    watched=extract_item_info(IDitem)["watched"]
-    watchlist=extract_item_info(IDitem)["watchlist"]
-    sold=extract_item_info(IDitem)["sold"]
-    coeff=round((watched+2*watchlist+3*sold)/6,2)
-    L_coeff.append(coeff)
+# for index, row in df.iterrows():
+#     IDitem = row["ID"].split('|')[1] 
+#     watched=extract_item_info(IDitem)["watched"]
+#     watchlist=extract_item_info(IDitem)["watchlist"]
+#     sold=extract_item_info(IDitem)["sold"]
+#     coeff=round((watched+2*watchlist+3*sold)/6,2)
+#     L_coeff.append(coeff)
 
-df["Coefficent"]=L_coeff
-df.to_csv('data.csv', index=False)
+# df["Coefficent"]=L_coeff
+# df.to_csv('data.csv', index=False)
 
 #Dictionnaire qui contient le classement pondéré de tout les sites
 Classements = {}
@@ -199,17 +200,19 @@ dict= [
 
 total_sum = {}
 count = {}
-    
+
 # Parcourir chaque dictionnaire dans la liste
 for d in dict:
     for key, value in d.items():
         # Ajouter la valeur au total de la clé et incrémenter le compteur
         total_sum[key] = total_sum.get(key, 0) + value
         count[key] = count.get(key, 0) + 1
-        weighted_avg = {key: int(total_sum[key] / count[key]) for key in total_sum}
 
-df['Rang'] = df['Marque'].map(weighted_avg)
-df.to_csv('data_cleaned.csv', index=False)
+# Calculer les moyennes pondérées et les convertir en entiers
+weighted_avg = {key: (total_sum[key] // count[key]) for key in total_sum}
+
+df3['Rang'] = df['Marque'].map(weighted_avg)
+df3.to_csv('data3.csv', index=False)
 
 
 
