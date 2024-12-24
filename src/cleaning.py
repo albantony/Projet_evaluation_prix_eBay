b84387a47d013a782b6206146ab02c811a3defe0
albantony <antony.albergne@ensae.fr>
2024-12-24 18:33:06 +0100

    partie cleaning dans le notebook

--- src/cleaning.py
+++ src/cleaning.py
@@ -226,7 +226,7 @@ def main():
     df = drop_columns(df, ['Largeur', 'Hauteur', 'Résolution','Code Couleur'])
     df['Date de publication'] = pd.to_datetime(df['Date de publication'], errors='coerce')
     #la colonne résolution est remplacée par la colonne PPI qui compile taille de l'écran et résolution
-    
+    print(df['Rang'].unique())
     #génération d'un nouveau csv pour les données nettoyées
     df.to_csv('data_semicleaned.csv', index=False)
 
