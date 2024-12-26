de5dc891e67c44f5ae2d150136cbbf041ece9d92
noelinecasteil <noeline.casteil@ensae.fr>
2024-12-26 16:49:05 +0100

    partie analyse et visualisation

--- src/graph.py
+++ src/graph.py
@@ -16,11 +16,11 @@ def plot_log_distribution(df, column):
 # 2) Prix en fonction de PPI
 def plot_price(df):
     # Supprimer les lignes avec des valeurs NaN dans 'Prix' ou 'Densité pixels'
-    #df_clean = df.dropna(subset=['Prix', 'PPI'])
+    df_clean = df.dropna(subset=['Prix', 'PPI'])
 
     # Création du graphique
     plt.figure(figsize=(10, 6))
-    plt.scatter(df['PPI'], df['Prix'], c='blue', alpha=0.5)
+    plt.scatter(df_clean['PPI'], df_clean['Prix'], c='blue', alpha=0.5)
     
     # Ajout des labels et du titre
     plt.title("Prix en fonction de la densité de pixels")
@@ -31,11 +31,17 @@ def plot_price(df):
     plt.grid(True)
     plt.show()
 
-def ppi_price(df):
+# Histogramme selon l'état 
+
+def etat(df):
+    conditions_counts = df['Condition'].value_counts()
     plt.figure(figsize=(10, 6))
-    plt.scatter(df['PPI'], df['Prix'], c='blue', alpha=0.5)
-    plt.title("Prix en fonction de la densité de pixels")
-    plt.xlabel("Densité de pixels (PPI)")
-    plt.ylabel("Prix (€)")
-    plt.grid(True)
+    plt.bar(conditions_counts.index, conditions_counts.values, color='skyblue')
+
+    plt.title("Répartition des ordinateurs portables par état (condition)", fontsize=16)
+    plt.xlabel("État", fontsize=14)
+    plt.ylabel("Nombre d'ordinateurs", fontsize=14)
+    plt.xticks(rotation=45, ha='right', fontsize=12)  # Inclinaison des labels pour éviter le chevauchement
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
     plt.show()
