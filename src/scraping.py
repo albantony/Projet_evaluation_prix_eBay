312078052bc1fc7e721a77ae5cf7f11eef1c2326
Adammmmmm16516 <belkhatiradam40@gmail.com>
2024-12-27 18:02:03 +0100

    ajustement coeff

--- src/scraping.py
+++ src/scraping.py
@@ -17,24 +17,28 @@ async def fetch_item_info(session, IDitem):
         html_content = str(page.findAll("span", class_="ux-textspans"))
 
         # Expression régulière pour extraire les informations pertinentes
-        watched_pattern = r"(\d+)\s*watched\s*in\s*the\s*last\s*24\s*hours"
+        watched_pattern = r"(\d+)\s*viewed\s*in\s*the\s*last\s*24\s*hours"
         watchlist_pattern = r"(\d+)\s*have\s*added\s*this\s*to\s*their\s*watchlist"
         sold_pattern = r"(\d+)\s*has\s*already\s*sold"
+        want_pattern = r"(\d+)\s*(?:person|people)\s*(?:is|are)\s*watching\s*this"  
 
         # Recherche des informations avec les expressions régulières
         watched_match = re.search(watched_pattern, html_content)
         watchlist_match = re.search(watchlist_pattern, html_content)
         sold_match = re.search(sold_pattern, html_content)
+        want_match = re.search(want_pattern, html_content)
 
         # Extraire les résultats trouvés
         watched_count = int(watched_match.group(1)) if watched_match else 0
         watchlist_count = int(watchlist_match.group(1)) if watchlist_match else 0
         sold_count = int(sold_match.group(1)) if sold_match else 0
+        want_count= int(want_match.group(1)) if want_match else 0
 
         return {
             "watched": watched_count,
             "watchlist": watchlist_count,
             "sold": sold_count,
+            "want": want_count
         }
 
 # Fonction pour traiter un lot de lignes
@@ -52,7 +56,8 @@ async def process_batch(df_batch):
             watched = result["watched"]
             watchlist = result["watchlist"]
             sold = result["sold"]
-            coeff = round((watched + 2 * watchlist + 3 * sold) / 6, 2)
+            want=result["want"]
+            coeff = round((watched + 2 * watchlist + 3 * sold + 2 * want ) / 8, 2)
             L_coeff.append(coeff)
     return L_coeff
 
@@ -65,10 +70,6 @@ def process_dataframe(df, batch_size=100):
         L_coeff = asyncio.run(process_batch(df_batch))
         df.loc[i:i+batch_size-1, "Coefficient"] = L_coeff
     df.to_csv('data_cleaned.csv', index=False)
-
-# Exemple d'utilisation
-df = pd.read_csv('data_cleaned.csv')  # Remplacez par votre fichier CSV
-process_dataframe(df)
 
 #Dictionnaire qui contient le classement pondéré de tout les sites
 Classements = {}
